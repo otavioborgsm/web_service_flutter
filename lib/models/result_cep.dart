404e8838ca8eade3bfe9b088c4522f7aa361fa22
ramonsl <ramonsl@gmail.com>
2021-11-11 13:48:55 -0300

    Classe Result

---
+++ lib/models/result_cep.dart
@@ -0,0 +1,62 @@
+// To parse this JSON data, do
+//
+//     final welcome = welcomeFromMap(jsonString);
+
+import 'package:meta/meta.dart';
+import 'dart:convert';
+
+class Welcome {
+  Welcome({
+    required this.cep,
+    required this.logradouro,
+    required this.complemento,
+    required this.bairro,
+    required this.localidade,
+    required this.uf,
+    required this.ibge,
+    required this.gia,
+    required this.ddd,
+    required this.siafi,
+  });
+
+  String cep;
+  String logradouro;
+  String complemento;
+  String bairro;
+  String localidade;
+  String uf;
+  String ibge;
+  String gia;
+  String ddd;
+  String siafi;
+
+  factory Welcome.fromJson(String str) => Welcome.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory Welcome.fromMap(Map<String, dynamic> json) => Welcome(
+    cep: json["cep"],
+    logradouro: json["logradouro"],
+    complemento: json["complemento"],
+    bairro: json["bairro"],
+    localidade: json["localidade"],
+    uf: json["uf"],
+    ibge: json["ibge"],
+    gia: json["gia"],
+    ddd: json["ddd"],
+    siafi: json["siafi"],
+  );
+
+  Map<String, dynamic> toMap() => {
+    "cep": cep,
+    "logradouro": logradouro,
+    "complemento": complemento,
+    "bairro": bairro,
+    "localidade": localidade,
+    "uf": uf,
+    "ibge": ibge,
+    "gia": gia,
+    "ddd": ddd,
+    "siafi": siafi,
+  };
+}
