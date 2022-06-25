83d7a5f00e3fc526c8981108f797fe8fc0160a19
Otávio Borges <otavio.demb@rede.ulbra.br>
2022-06-25 13:36:28 -0300

    get cep com pacote dio

--- lib/services/via_cep_service.dart
+++ lib/services/via_cep_service.dart
@@ -1,13 +1,13 @@
-
-import 'package:http/http.dart' as http;
 import 'package:web_service/models/result_cep.dart';
+import 'package:dio/dio.dart';
+var dio = Dio();
 
 class ViaCepService {
   static Future<ResultCep> fetchCep({String? cep}) async {
     final Uri uri = Uri.parse('https://viacep.com.br/ws/$cep/json/');
-    final response = await http.get(uri);
+    final response = await dio.getUri(uri);
     if (response.statusCode == 200) {
-      return ResultCep.fromJson(response.body);
+      return ResultCep.fromJson(response.toString());
     } else {
       throw Exception('Requisição inválida!');
     }
